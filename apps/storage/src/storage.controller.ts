b411979dc2d36fef888e64a2d956e062d7117a22
codelab-kr <master@code-lab.kr>
2024-02-10 20:15:17 +0900

    test jenkins

--- apps/storage/src/storage.controller.ts
+++ apps/storage/src/storage.controller.ts
@@ -1,8 +1,6 @@
 import { Controller, Post, Req, Res } from '@nestjs/common';
 import { StorageService } from './storage.service';
 import { Request, Response } from 'express';
-import * as path from 'path';
-import { createWriteStream } from 'fs';
 
 @Controller()
 export class StorageController {
@@ -11,18 +9,7 @@ export class StorageController {
   @Post('/upload')
   UploadVideo(@Req() req: Request, @Res() res: Response) {
     console.log('Uploading video...');
-    const localFilePath = path.resolve(
-      `public/uploads/videos/${req.headers.path.toString()}`,
-    );
-    const fileWriteStream = createWriteStream(localFilePath);
-    req
-      .pipe(fileWriteStream)
-      .on('error', (err) => {
-        console.error('Upload failed.');
-        console.error((err && err.stack) || err);
-      })
-      .on('finish', () => {
-        res.sendStatus(200);
-      });
+
+    this.storageService.uploadVideo(req, res);
   }
 }
--- apps/storage/src/storage.service.ts
+++ apps/storage/src/storage.service.ts
@@ -1,4 +1,44 @@
 import { Injectable } from '@nestjs/common';
+import { join, resolve } from 'path';
+import { createWriteStream } from 'fs';
+import { existsSync, mkdirSync } from 'fs';
 
 @Injectable()
-export class StorageService {}
+export class StorageService {
+  uploadVideo(req: any, res: any) {
+    console.log('Uploading video...');
+    const contentType = req.headers['content-type'].toString();
+    const uploadPath = this.getUploadPath(contentType);
+    const path = req.headers.path.toString();
+    const localFilePath = join(uploadPath, path);
+    console.log('localFilePath', localFilePath);
+    const fileWriteStream = createWriteStream(localFilePath);
+    req
+      .pipe(fileWriteStream)
+      .on('error', (err) => {
+        console.error('Upload failed.');
+        console.error((err && err.stack) || err);
+      })
+      .on('finish', () => {
+        res.sendStatus(200);
+      });
+  }
+
+  getUploadPath = (contentType: string): string => {
+    let uploadPath: string;
+    console.log('contentType', contentType);
+    console.log('__dirname', __dirname);
+    if (contentType.match(/\/(mp4)$/)) {
+      uploadPath = resolve('./public/uploads/videos');
+    } else if (contentType.match(/\/(jpg|jpeg|png)$/)) {
+      uploadPath = resolve('./public/uploads/images');
+    } else {
+      uploadPath = resolve('./public/uploads/files');
+    }
+    if (!existsSync(uploadPath)) {
+      console.log('uploadPath', uploadPath);
+      mkdirSync(uploadPath, { recursive: true });
+    }
+    return uploadPath;
+  };
+}
